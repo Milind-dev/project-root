a3e30f0f3831dbd90f55de6878073cb7f837c2f4
Milind-dev <milinddev1011@gmail.com>
2025-11-29 12:08:00 +0530

    setup

---
+++ frontend/matbook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  // console.log(process.env)
+  // console.log("processs", process.env.REACT_APP_API_URL);
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-red-700">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
